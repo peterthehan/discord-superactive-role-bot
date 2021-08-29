068e36e466535a51f7d248dffff074941fe54d62
Peter Han <peter.han.9493@gmail.com>
2021-08-28 17:34:04 -0700

    Add presence intent

--- src-discord-superactive-role-bot/intents.ts
+++ src-discord-superactive-role-bot/intents.ts
@@ -1,3 +1,7 @@
 import { Intents } from "discord.js";
 
-module.exports = [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS];
+module.exports = [
+  Intents.FLAGS.GUILDS,
+  Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+  Intents.FLAGS.GUILD_PRESENCES,
+];
