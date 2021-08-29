98fad6d7b9e39b932df33696d57b9a5b9ef3a69b
Peter Han <peter.han.9493@gmail.com>
2021-08-28 18:53:13 -0700

    Remove intents

--- src-discord-superactive-role-bot/intents.ts
+++ src-discord-superactive-role-bot/intents.ts
@@ -1,7 +1,1 @@
-import { Intents } from "discord.js";
-
-module.exports = [
-  Intents.FLAGS.GUILDS,
-  Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-  Intents.FLAGS.GUILD_PRESENCES,
-];
+module.exports = [];
